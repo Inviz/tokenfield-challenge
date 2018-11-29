5195a1c18ac672564c14d2eb86316af567de8463
Yaroslaff Fedin <invizko@gmail.com>
2018-11-29 19:22:04 +0800

    Add build

---
+++ build/precache-manifest.003df9f934bcfa6cd94ce5261232bc26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b0c7e515b186114007a",
+    "url": "/static/js/main.1b0c7e51.chunk.js"
+  },
+  {
+    "revision": "bcd660d22b8d57d77fa2",
+    "url": "/static/js/1.bcd660d2.chunk.js"
+  },
+  {
+    "revision": "1b0c7e515b186114007a",
+    "url": "/static/css/main.11e14ab0.chunk.css"
+  },
+  {
+    "revision": "1b18390af098a3a9f7d2e3fe4a17f6a5",
+    "url": "/index.html"
+  }
+];
