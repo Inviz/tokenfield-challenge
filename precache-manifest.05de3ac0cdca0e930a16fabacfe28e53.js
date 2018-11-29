1156c538904fc08013101840befef893da8c4b03
Yaroslaff Fedin <invizko@gmail.com>
2018-11-29 19:47:23 +0800

    Updates

---
+++ precache-manifest.05de3ac0cdca0e930a16fabacfe28e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb158ab64ed266b995914babb7ecf46a",
+    "url": "/tokenfield-challenge/static/media/expand.bb158ab6.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/tokenfield-challenge/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "dfe27d9db61510e3614a",
+    "url": "/tokenfield-challenge/static/js/runtime~main.dfe27d9d.js"
+  },
+  {
+    "revision": "7676c6fc6ad198c99bb6",
+    "url": "/tokenfield-challenge/static/js/main.7676c6fc.chunk.js"
+  },
+  {
+    "revision": "bcd660d22b8d57d77fa2",
+    "url": "/tokenfield-challenge/static/js/1.bcd660d2.chunk.js"
+  },
+  {
+    "revision": "7676c6fc6ad198c99bb6",
+    "url": "/tokenfield-challenge/static/css/main.11c9abd3.chunk.css"
+  },
+  {
+    "revision": "7a15509bd02238fe90cc229a12814c9d",
+    "url": "/tokenfield-challenge/index.html"
+  }
+];
